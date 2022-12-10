814900a4f1ce1d1de67f4baea107f578b8691618
Hazem <hazem.muhammad.96@gmail.com>
2022-12-10 17:29:54 +0200

    prisma init

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,39 @@
 import commonTest from "common";
-import initApp from "./Server";
+import initApp from "./server";
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 initApp();
+
+async function main() {
+
+    await prisma.user.create({
+        data: {
+            name: 'Alice2',
+            email: 'alice2@prisma.io',
+            posts: {
+                create: { title: 'Hello World' },
+            },
+            profile: {
+                create: { bio: 'I like turtles' },
+            },
+        },
+    })
+
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+            profile: true,
+        },
+    })
+    console.dir(allUsers, { depth: null })
+
+}
+
+main().then(async () => await prisma.$disconnect())
+    .catch(async e => {
+        console.log("Prisma Error:\n", e);
+        await prisma.$disconnect();
+        process.exit(1);
+    })
