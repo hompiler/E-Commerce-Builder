19390edf837edb23ac09ef43044951d754707833
ziad tarek <66745488+ziadtarek112@users.noreply.github.com>
2022-12-12 02:08:03 +0200

    CRUD completed

--- apps/api/src/trader/routes/controller.ts
+++ apps/api/src/trader/routes/controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+
 import prisma from "../../db";
 export async function addTrader(req: Request, res: Response, next: NextFunction) {
     const createdTrader = await prisma.trader.create(
@@ -28,35 +29,78 @@ export async function addTrader(req: Request, res: Response, next: NextFunction)
         createdTrader
     );
 }
+
+
+
 export async function getAllTraders(req: Request, res: Response, next: NextFunction) {
 
     const allTraders = await prisma.trader.findMany();
     res.status(200).json(allTraders);
 
 }
+
+
 export async function getTraderById(req: Request, res: Response, next: NextFunction) {
-    const trader = await prisma.trader.findUnique({where : {
-        id: req.params.id
-
-    }
-
-});
+    const trader = await prisma.trader.findUnique({
+        where: {
+            id: req.params.id
+        }
+    });
     res.status(200).json(trader);
 
 }
-export async function updateTrader(req: Request, res: Response, next: NextFunction)
-{
+
+
+export async function updateTrader(req: Request, res: Response, next: NextFunction) {
+
+    const trader = await prisma.trader.findUnique({
+        where: {
+            id: req.params.id
+        }
+    });
+    const updatedTraderProfile = await prisma.profile.update({
+        where: {
+            id: trader.profileId
+        },
+        data: req.body.profile
+    })
+
     const updatedTrader = await prisma.trader.update({
-        where :{
+        where: {
             id: req.params.id
 
         },
-        data:{
-            
-            ...req.body
+        data: {
+            ssn: req.body.ssn,
+            policy: req.body.policy,
+            description: req.body.description,
         }
-
     });
-    res.status(200).json(updatedTrader);
 
+
+
+    res.status(201).json(updatedTrader);
+
+}
+export async function deleteTrader(req: Request, res: Response, next: NextFunction) {
+    const trader = await prisma.trader.findUnique({
+        where: {
+            id: req.params.id
+        }
+    });
+    const deleteUser = await prisma.trader.delete({
+        where : {
+            id : req.params.id
+        }
+    })
+    const deletedTraderProfile = await prisma.profile.delete({
+        where: {
+            id: trader.profileId
+        }
+    })
+  
+
+    res.status(204).json({
+        message : "deleted successfully"
+    })
 }
--- apps/api/src/trader/routes/trader.ts
+++ apps/api/src/trader/routes/trader.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addTrader, getAllTraders, getTraderById, updateTrader } from "./controller";
+import { addTrader, deleteTrader, getAllTraders, getTraderById, updateTrader } from "./controller";
 
 export const router = Router();
 router.post('/',addTrader);
 router.get('/',getAllTraders);
 router.get('/:id',getTraderById);
 router.patch('/:id',updateTrader);
+router.delete('/:id',deleteTrader)
