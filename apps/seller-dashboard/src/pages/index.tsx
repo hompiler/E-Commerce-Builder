87549c123fc83c8e779ff01183b2a8dd5a370f84
Hazem Muhammad Abdelfattah <hazem.muhammad.96@gmail.com>
2023-03-27 01:52:52 +0200

    api client

--- apps/seller-dashboard/src/pages/index.tsx
+++ apps/seller-dashboard/src/pages/index.tsx
@@ -1,17 +1,4 @@
-import { useEffect } from "react";
-import ApiClient from "@api-client";
-
 export default function Home() {
-    useEffect(() => {
-        ApiClient.getRequest("https://mock.codes/400", {
-            passError: true,
-        })
-            .then((res) => {
-                console.log({ res });
-            })
-            .catch((err) => {
-                console.log({ err });
-            });
-    }, []);
+
     return <div>Hello MarketMate</div>;
 }
