51feb8d2cfba7bda0fe96138663acfd41c6398ff
Hazem Muhammad Abdelfattah <hazem.muhammad.96@gmail.com>
2023-07-04 04:21:19 +0300

    login & register designs

--- apps/seller-dashboard/src/features/studio/presntation/layout/Nav/Nav.tsx
+++ apps/seller-dashboard/src/features/studio/presntation/layout/Nav/Nav.tsx
@@ -2,7 +2,12 @@
 import React from "react";
 import styles from "./Nav.module.scss";
 import { IconButtonGroup } from "@lib/components/buttons/IconButton/IconButtonGroup";
-import { SettingsIcon, LayersIcon, PageIcon, PlusIcon } from "@icons/index";
+import {
+    SettingsLightIcon as SettingsIcon,
+    LayersIcon,
+    PageIcon,
+    PlusIcon,
+} from "@icons/index";
 
 export default function Nav({ className, selectedPane, setSelectedPane }: any) {
     return (
